9cd1ce3b369a784b593bda4acea4ee1b14ad1e3b
coolfever008 <51772725+coldfever008@users.noreply.github.com>
2019-08-26 12:34:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const riot = require('matrix-js-sdk');
-const webhook = require('webhook-discord'); //
-const hook = new webhook.Webhook('')
-const userid = '@nameofbot:matrix.org';
-const accesstoken = '';
+const {accesstoken,userid,prefix} = require('./config.json');
+
 const client = riot.createClient({
   baseUrl: 'https://matrix.org',
   accessToken: accesstoken,
@@ -10,26 +8,18 @@ const client = riot.createClient({
 });
 
 client.once('sync', function(state, prevState, res) {
-  if(state=='PREPARED') return console.log("I am up!");
+  if(state=='PREPARED') return console.log('I am running...');
 });
 
 client.on('Room.timeline', function(event, room, toStartOfTimeline) {
   if(event.getType()!=='m.room.message') return;
-  hook.info('riot',`(${room.name}) | ${event.getSender()}: ${event.getContent().body}`)
-  const args = event.event.content.body.slice('^'.length).split(' ');
-  if(event.event.content.body.includes('^sendmessage')) {
-    client.sendEvent(room.roomId, 'm.room.message', { 'body': 'ok sure', 'msgtype': 'm.text'}, '', (err, res) => {hook.info('riot demands', args.slice(1).join(' ')) });
-  }
-  switch(event.event.content.body) {
-    case "bot help":
-      client.sendEvent(room.roomId, 'm.room.message', { 'body': 'bot version, bot help', 'msgtype': 'm.text'}, '', (err, res) => { console.log(err) });
-      break;
-    case "bot version":
-      client.sendEvent(room.roomId, 'm.room.message', { 'body': '0.1 beta', 'msgtype': 'm.text'}, '', (err, res) => { console.log(err) });
-      break;
-      
+  
+  const args = event.event.content.body.slice(prefix.length).split(' ');
+  
+  if(event.event.content.body.startsWith(prefix+'ping')) {
+    client.sendEvent(room.roomId, 'm.room.message', { 'body': 'Pong!', 'msgtype': 'm.text'}, '', (err, res) => { });
+  } else if(event.event.content.body.startsWith(prefix+'arguments')) {
+    client.sendEvent(room.roomId, 'm.room.message', { 'body': args.split(1).join(','), 'msgtype': 'm.text'}, '', (err, res) => { });
   }
 });
 client.startClient();
-console.log("Bot is starting...")
-//get the token by doing localStorage.getItem("mx_access_token") in the console.
